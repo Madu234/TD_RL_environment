6ef52fab3aa1f74a2379cdde9920304b2e6bd7da
Madu234 <79806642+Madu234@users.noreply.github.com>
2023-07-05 23:49:42 +0300

    Initial version v0.1

--- Game/Game.py
+++ Game/Game.py
@@ -1,4 +1,5 @@
 import pygame
+from enemy import Enemy
 
 
 class TowerDefenseGame:
@@ -14,12 +15,17 @@ class TowerDefenseGame:
         self.GREY = (200, 200, 200)
         self.RED = (255, 0, 0)
         self.BLUE = (0, 0, 255)
+        self.GREEN = (0, 255, 0)
 
         self.WIN = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
 
         self.grid = [["" for _ in range(self.GRID_SIZE)] for _ in range(self.GRID_SIZE)]
 
+        self.enemies = [Enemy(0, 0, self.CELL_SIZE // 2, self.CELL_SIZE // 2, self.GREEN, 2, self.CELL_SIZE)]
+
+
     def draw_grid(self):
+        self.WIN.fill(self.WHITE)  # Add this line
         for i in range(self.GRID_SIZE):
             for j in range(self.GRID_SIZE):
                 rect = pygame.Rect(j * self.CELL_SIZE, i * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE)
@@ -29,6 +35,20 @@ class TowerDefenseGame:
                     pygame.draw.rect(self.WIN, self.BLUE, rect)
                 else:
                     pygame.draw.rect(self.WIN, self.GREY, rect, 1)
+
+    def draw_enemies(self):
+        for enemy in self.enemies:
+            enemy.draw(self.WIN)
+
+    def update_enemies(self):
+        for enemy in self.enemies:
+            enemy.move()
+
+        # Remove enemies that have moved off the screen or reached the end
+        self.enemies = [enemy for enemy in self.enemies if 0 <= enemy.cell_y < self.GRID_SIZE-1]
+
+        # Remove enemies that have moved off the screen
+        self.enemies = [enemy for enemy in self.enemies if 0 <= enemy.y <= self.HEIGHT]
 
     def main(self):
         clock = pygame.time.Clock()
@@ -51,7 +71,10 @@ class TowerDefenseGame:
                         if self.grid[i][j] == "":
                             self.grid[i][j] = "wall"
 
+            self.update_enemies()
             self.draw_grid()
+            self.draw_enemies()
+
             pygame.display.update()
 
         pygame.quit()
